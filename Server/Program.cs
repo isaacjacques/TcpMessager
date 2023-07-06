915afde37356fbef894237c0ee54a5b8c0fdf019
isaacjacques <you@example.comname>
2023-07-05 20:18:09 -0400

    init

---
+++ Server/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Net;
+using TcpMessager;
+
+IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+int port = 999;
+
+TcpServer server = new TcpServer(ipAddress, port);
+server.Start();
+
+Console.WriteLine("Press Enter to stop the server...");
+Console.ReadLine();
+
+server.Stop();
---
+++ TcpMessager/TcpServer.cs
@@ -0,0 +1,82 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace TcpMessager
+{
+    public class TcpServer
+    {
+        private readonly IPAddress ipAddress;
+        private readonly int port;
+        private TcpListener listener;
+        private bool isRunning;
+
+        public TcpServer(IPAddress ipAddress, int port)
+        {
+            this.ipAddress = ipAddress;
+            this.port = port;
+        }
+
+        public void Start()
+        {
+            listener = new TcpListener(ipAddress, port);
+            listener.Start();
+            isRunning = true;
+
+            Console.WriteLine("Server started. Listening for incoming connections...");
+
+            Task.Run(async () =>
+            {
+                while (isRunning)
+                {
+                    TcpClient client = await listener.AcceptTcpClientAsync();
+                    _ = HandleClientAsync(client);
+                }
+            });
+        }
+
+        public void Stop()
+        {
+            isRunning = false;
+            listener.Stop();
+            Console.WriteLine("Server stopped.");
+        }
+
+        private async Task HandleClientAsync(TcpClient client)
+        {
+            Console.WriteLine($"Client connected: {client.Client.RemoteEndPoint}");
+
+            try
+            {
+                NetworkStream stream = client.GetStream();
+                byte[] buffer = new byte[4096];
+
+                while (isRunning)
+                {
+                    int bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length);
+
+                    if (bytesRead == 0)
+                    {
+                        break;
+                    }
+
+                    string request = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                    Console.WriteLine($"Received from {client.Client.RemoteEndPoint}: {request}");
+
+                    string response = "Hello from the server!";
+                    byte[] responseBytes = Encoding.UTF8.GetBytes(response);
+                    await stream.WriteAsync(responseBytes, 0, responseBytes.Length);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error occurred for client {client.Client.RemoteEndPoint}: {ex.Message}");
+            }
+            finally
+            {
+                Console.WriteLine($"Client disconnected: {client.Client.RemoteEndPoint}");
+                client.Close();
+            }
+        }
+    }
+}
